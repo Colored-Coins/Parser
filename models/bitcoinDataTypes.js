b04f3d62d1d2721586d079b1001231997f2419f3
Oded Leiba <oded@colu.co>
2016-04-27 12:38:03 +0300

    fix

--- models/bitcoinDataTypes.js
+++ models/bitcoinDataTypes.js
@@ -1,4 +1,4 @@
-var Sequelize = require('Sequelize')
+var Sequelize = require('sequelize')
 
 module.exports = {
 	hashType: Sequelize.STRING(64),	// even though this could be of type character (n), there is no performance advantage for this type. In fact, its the slowest char type.
